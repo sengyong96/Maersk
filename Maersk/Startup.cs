065c65fe1edefc87788516b0a9fc0b60d69b8cc8
User's PC <User's PC@DESKTOP-8DOIISO>
2018-01-13 15:47:05 +0800

    Add project files.

---
+++ Maersk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Maersk.Startup))]
+namespace Maersk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
